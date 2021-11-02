5164a881402057a31494a978f4341abf43b85760
flofriday <flohacksfriday@gmail.com>
2021-11-02 15:34:01 +0100

    Add a custom ansi escape code parser

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # error-gallery
+
+Show how different compilers tell you that you called a function `hallo`
+that doesn't exist.
+
+## Generate the index file
+
+**WIP:** This is currently very specific to my machine, but I will create a
+Dockerfile in the future.
+
+You need to install go, gcc, clang, and rust and then run `go run generate.go`.
--- generate.go
+++ generate.go
@@ -11,7 +11,6 @@ import (
 
 	"github.com/BurntSushi/toml"
 	"github.com/creack/pty"
-	"github.com/jwalton/go-ansiparser"
 )
 
 type Config struct {
@@ -34,7 +33,6 @@ func (l *Language) Prepare() {
 func executeCmd(command string) (string, error) {
 	// TODO: This should return an error if the programm cannot be found but
 	// should be fine with the command returning not null
-
 	parts := strings.Split(command, " ")
 	cmd := exec.Command(parts[0], parts[1:]...)
 	f, _ := pty.Start(cmd)
@@ -42,50 +40,130 @@ func executeCmd(command string) (string, error) {
 	return strings.TrimSpace(string(output)), nil
 }
 
-func ansiToHTML(ansi string) template.HTML {
-	// Escape the output to avoid injections
-	ansi = template.HTMLEscapeString(ansi)
+func ansiEscToHTML(style *Formatting, funcName string, params string) string {
+	if funcName != "m" {
+		fmt.Println("Unknown function:", funcName, params)
+		return ""
+	}
 
-	// Parse the ansi
-	tokenizer := ansiparser.NewStringTokenizer(ansi)
+	// A article on what those numbers mean can be found here
+	// https://notes.burke.libbey.me/ansi-escape-codes/
+	ansiTable := map[string]func(){
+		"":  style.Reset,
+		"0": style.Reset,
+		"1": func() { style.Bold = true },
+		"3": func() { style.Italic = true },
+		"4": func() { style.Underline = true },
 
-	ansiTable := map[string]string{
-		"":   "",
-		"30": "",
-		"31": "red",
-		"32": "green",
-		"33": "yellow",
-		"34": "blue",
-		"35": "purple",
+		"30": func() { style.Color = "" },
+		"31": func() { style.Color = "red" },
+		"32": func() { style.Color = "green" },
+		"33": func() { style.Color = "yellow" },
+		"34": func() { style.Color = "blue" },
+		"35": func() { style.Color = "purple" },
 
 		// TODO: fix this:
 		// Hardcode the most common ansi256 colors
-		"38;5;9":  "red",
-		"38;5;10": "green",
-		"38;5;11": "yellow",
-		"38;5;12": "blue",
+		"38;5;9":  func() { style.Color = "red" },
+		"38;5;10": func() { style.Color = "green" },
+		"38;5;11": func() { style.Color = "yellow" },
+		"38;5;12": func() { style.Color = "blue" },
 	}
 
-	// TODO: this is ugly .. fix it
-	result := "<span>"
-	for tokenizer.Next() {
-		token := tokenizer.Token()
-		if token.Type == ansiparser.String {
-			result += token.Content
+	args := strings.Split(params, ";")
+	if len(args) == 0 {
+		args = append(args, "")
+	}
+
+	current := make([]string, 0)
+	for _, arg := range args {
+		arg = strings.TrimLeft(arg, "0")
+		current = append(current, arg)
+		callback, ok := ansiTable[strings.Join(current, ";")]
+		if !ok {
 			continue
 		}
 
-		// token is a command
-		class, ok := ansiTable[token.FG]
-		if !ok {
-			fmt.Printf("%+v\n", token.FG)
-			class = ""
+		callback()
+		current = nil
+	}
+
+	if current != nil {
+		fmt.Printf("Unknown ansi code: [%sm\n", strings.Join(current, ";"))
+	}
+
+	return style.GenerateHTML()
+}
+
+type Formatting struct {
+	Color     string
+	Bold      bool
+	Italic    bool
+	Underline bool
+}
+
+func (f *Formatting) Reset() {
+	f.Color = ""
+	f.Bold = false
+	f.Italic = false
+	f.Underline = false
+}
+
+func (f *Formatting) GenerateHTML() string {
+	classes := []string{}
+	if f.Color != "" {
+		classes = append(classes, f.Color)
+	}
+	if f.Bold {
+		classes = append(classes, "bold")
+	}
+	if f.Italic {
+		classes = append(classes, "italic")
+	}
+	if f.Underline {
+		classes = append(classes, "underline")
+	}
+
+	return fmt.Sprintf("</span><span class=\"%s\">", strings.Join(classes, " "))
+}
+
+func parseAnsiText(ansi string) string {
+
+	style := Formatting{}
+	result := ""
+	escState := false
+	escSeq := ""
+	for _, c := range []byte(ansi) {
+		if !escState && c != '\x1b' {
+			result += string(c)
+			continue
+		}
+		if !escState && c == '\x1b' {
+			escState = true
+			continue
 		}
 
-		result += fmt.Sprintf("</span><span class=\"%s\">", class)
+		if strings.Contains("ABCDEFGHIJKSTsufm", string(c)) {
+			if escSeq[0] == '[' {
+				result += ansiEscToHTML(&style, string(c), escSeq[1:])
+			}
+			escState = false
+			escSeq = ""
+			continue
+		}
+		escSeq += string(c)
 	}
-	result += "</span>"
 
+	return result
+}
+
+// https://notes.burke.libbey.me/ansi-escape-codes/
+func ansiToHTML(ansi string) template.HTML {
+	// Escape the output to avoid injections
+	ansi = template.HTMLEscapeString(ansi)
+	result := "<span>"
+	result += parseAnsiText(ansi)
+	result += "</span>"
 	return template.HTML(result)
 }
 
